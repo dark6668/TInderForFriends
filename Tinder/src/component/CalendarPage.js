38ee469652423527d5d1d158664417f778c723cf
dark6668 <sus.sus123456@gmail.com>
2023-09-11 08:53:33 +0300

    Try

--- Tinder/src/component/CalendarPage.js
+++ Tinder/src/component/CalendarPage.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect} from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 import FetchRequest from "./FetchRequest";
 import { API_URL } from "@env";
+import messaging from '@react-native-firebase/messaging';
+
 
 export default function CalendarPage(props) {
-	useEffect(() => {
-		getYourActivity();
-	}, []);
 
 	const [selected, setSelected] = React.useState("");
 	const [yourEvent, setYourEvent] = React.useState([]);
+
+	useEffect(() => {
+		getYourActivity();
+		requestUserPermission()
+	}, []);
+	async function requestUserPermission() {
+		const authStatus = await messaging().requestPermission();
+		const enabled =
+		  authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+		  authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+	  
+		if (enabled) {
+		  console.log('Authorization status:', authStatus);
+		}
+	  }
 	const getYourActivity = async () => {
+	
 		const id = {
 			id: props.userInfo.id,
 		};
@@ -25,7 +40,7 @@ export default function CalendarPage(props) {
 			setYourEvent(data);
 		});
 	};
-
+	
 	return (
 		<View style={styles.container}>
 			<Calendar
@@ -36,33 +51,50 @@ export default function CalendarPage(props) {
 					[selected]: { selected: true, disableTouchEvent: true },
 				}}
 			/>
-			<View>
-				{yourEvent.length > 0 && (
-					<View>
-						{yourEvent.map((item) => {
-							if (selected === item.date.split(" ")[0]) {
-								return (
-									<React.Fragment key={item.id}>
-										<Text>Event: {item.activity}</Text>
-										<Text>Location: {item.location}</Text>
-										<Text>Date: {item.date.split(" ")[0]}</Text>
-										<Text>Time: {item.date.split(" ")[1].slice(0, 5)}</Text>
-										<Text>Organizer: {item.event_organizer}</Text>
-									</React.Fragment>
-								);
-							} else {
-								return null;
-							}
-						})}
-					</View>
-				)}
-			</View>
+<View>
+  {yourEvent.length > 0 && (
+    <View style={styles.containerEvent}>
+      {yourEvent.map((item) =>
+        selected === item.date.split(" ")[0] ? (
+          <View key={item.date + item.id} style={styles.event}>
+            <Text  style={styles.text}>Event: {item.activity}</Text>
+            <Text  style={styles.text}>Location: {item.location}</Text>
+            <Text  style={styles.text}>Date: {item.date.split(" ")[0]}</Text>
+            <Text  style={styles.text}>Time: {item.date.split(" ")[1].slice(0, 5)}</Text>
+            <Text  style={styles.text}>Organizer: {item.event_organizer}</Text>
+          </View>
+        ) : null
+      )}
+    </View>
+  )}
+</View>
 		</View>
 	);
 }
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "white",
+
+
+			height: "100%",
+			backgroundColor: "#a2e2e2",
+
 	},
+	event: {
+		display: "flex",
+		alignItems: "center",
+	borderColor:'red',
+
+	borderWidth:1,
+	
+		
+	},
+	text:{
+
+	},
+	containerEvent:{
+		display:"flex",
+		flexDirection:"column",
+		justifyContent:"space-evenly",
+		height:300
+	}
 });
